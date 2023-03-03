59b507a3a2b39105ec62ca359a4f9a297db81be0
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-03-03 15:15:12 -0800

    preparing the setup function for Firebase

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,45 @@
 // Include Firebase library
 #include <Firebase_ESP_Client.h>
 
+// Other dependencies
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-// config.host = FIREBASE_HOST;
-// config.api_key = API_KEY;
 
-// auth.user.email = USER_EMAIL;
-// auth.user.password = USER_PASSWORD;
-
-// Firebase.begin(&config, &auth);
-
-// Firebase.reconnectWiFi(true);
+bool signedUp = false;
 
 void setup(){
   Serial.begin(115200);
+  WiFi.begin(std::getenv("WIFI_SSID"), std::getenv("WIFI_PASSWORD"));
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+
+  config.api_key = std::getenv("API_KEY");
+  config.database_url = std::getenv("DATABASE_URL");
+
+  auth.user.email = std::getenv("USER_EMAIL");
+  auth.user.password = std::getenv("USER_PASSWORD");
+
+  if (Firebase.signUp(&config, &auth, auth.user.email, auth.user.password)) {
+    Serial.print("Sign up was a success.");
+    signedUp = true;
+  } else {
+    Serial.print(config.signer.signupError.message.c_str());
+  }
+
+  config.token_status_callback = tokenStatusCallback;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop(){
+
 }
